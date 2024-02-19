0c0641d6a752704da8618efddf1d669303757ec2
Omer Gaiznger <Omerg863@gmail.com>
2024-02-19 16:12:05 +0200

    course page

--- src/pages/course.jsx
+++ src/pages/course.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function course() {
+function course({ activeTab }) {
   return (
-    <div>course</div>
+    <main>
+      <div  className="underline" >
+        <h1>{activeTab.title}</h1>
+      </div>
+      <div>
+        {activeTab.description.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>
+        })}
+      </div>
+      <div className='flex-center'>
+        <img src={activeTab.certificate} alt={"certificate"} style={{width: "70%", marginLeft: "auto", marginRight: "auto"}}/>
+      </div>
+    </main>
   )
 }
 
